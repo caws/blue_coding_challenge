d8b3d4e96c6e6b1d0f93696097a9c19678571381
Charles Washington <charles_was_7@hotmail.com>
2019-09-25 19:12:05 -0300

    Fix factories. Remove unnecessary specs. Add method to shorten_url and update the attribute.

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,20 @@
 class Url < ApplicationRecord
+  after_create_commit :create_short_url
+  validates_presence_of :full_url
+  validates_uniqueness_of :full_url
+
+  private
+
+  def create_short_url
+    update(short_url: shorten_url)
+  end
+
+  # Method below converts a given id
+  # to a base36 value.
+  #
+  # It will generate short urls of lenght
+  # <= 5 up until the ID is < 9999999
+  def shorten_url
+    id.to_s(36)
+  end
 end
