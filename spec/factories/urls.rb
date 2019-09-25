1f6471bbee7113b1ea8cc9ff1ecf7563c7a14ef5
Charles Washington <charles_was_7@hotmail.com>
2019-09-25 17:38:27 -0300

    Add base url_model model to project. Set hit_counter to 0 by default. Create base factory and add base model test

---
+++ spec/factories/urls.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :url do
+    short_url { "MyString" }
+    full_url { "MyString" }
+    page_title { "MyString" }
+    hit_counter { 1 }
+  end
+end
---
+++ spec/models/url_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Url, type: :model do
+  RSpec.describe Recipe, type: :model do
+    let(:url) { build(:url) }
+
+    describe 'ActiveModel validations' do
+    end
+  end
+end
