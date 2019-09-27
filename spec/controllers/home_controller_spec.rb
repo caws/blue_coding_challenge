c9681afb585cb7f868d256ca9b3c95f57135d00b
Charles Washington <charles_was_7@hotmail.com>
2019-09-26 23:12:18 -0300

    Add coverage gem and more test specs that cover helpers and controllers.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,19 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-  # describe "GET #index" do
-  #   it "returns http success" do
-  #     get :index
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
-  # describe "GET #most_popular" do
-  #   it "returns http success" do
-  #     get :most_popular
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET #visit_shortened_url" do
+    it "should enter redirection page" do
+      base_url = Url.first
+      get :visit_shortened_url, params: {short_url: base_url.short_url}
+      expect(assigns(:url)).to eq(base_url)
+    end
+  end
+
+  describe "GET #most_popular" do
+    it "returns http success" do
+      get :most_popular
+      expect(response).to have_http_status(:success)
+    end
+
+    it "assigns @most_popular_urls" do
+      get :most_popular, params: {qty: 100}
+      most_popular_urls = Url.most_popular
+      expect(assigns(:most_popular_urls)).to eq(most_popular_urls)
+    end
+  end
+
+  describe "POST #create_shortened_url" do
+    context 'with valid input' do
+      it "should create a valid url record" do
+        post :create_shortened_url, params: {url: {full_url: 'https://test.com'}}
+        expect(assigns(:url).valid?).to be(true)
+      end
+    end
+
+    context 'with invalid input' do
+      it "should not create a valid url record" do
+        post :create_shortened_url, params: {url: {full_url: 'test.com'}}
+        expect(assigns(:url).valid?).to be(false)
+      end
+    end
+  end
+
 
 end
